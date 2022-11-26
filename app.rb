1a0dfa7cd1bd75acd604b3aebfed40649bc48c23
leandrosardi <leandro.sardi@expandedventure.com>
2022-11-26 18:24:18 -0300

    #1

--- app.rb
+++ app.rb
@@ -9,9 +9,6 @@ end
 get '/progressjs/superhero_secret.json' do
     erb :"/extensions/progressjs/views/superhero_secret"
 end
-post '/progressjs/superhero_secret.json' do
-    erb :"/extensions/progressjs/views/superhero_secret"
-end
 
 =begin
 get '/progressjs', :agent => /(.*)/ do
